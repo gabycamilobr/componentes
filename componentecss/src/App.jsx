e1034a4d5f81b122d6378c767d408b932c6e9959
Gabrielly Barbosa Camilo <gabycamilobr@gmail.com>
2024-09-04 12:19:22 -0300

    Componentes

---
+++ componentecss/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+  <div className="box flex flex-row h-50 w-full p-14 bg-pink-600"></div>
+  <div className="box h-screen flex flex-col w-32 p-20 bg-green-300 items-center justify-around">
+    <div className="flex h-24 w-24  bg-pink-600"></div>
+    <div className="flex h-24 w-24  bg-pink-600"></div>
+    <div className="flex h-24 w-24  bg-pink-600"></div>
+  </div>
+    </>
+  )
+}
+
+export default App
